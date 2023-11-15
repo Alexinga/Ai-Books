f9006da3af4f621f27abd0b72fd5c9187397b52b
Alex Inga <alexinga98@gmail.com>
2023-11-15 11:28:21 -0500

    Added new logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,11 +2,6 @@ import { NavLink } from "react-router-dom";
 
 function Logo() {
   return (
-    // <img
-    //   className={styles.img}
-    //   src="/images/ai-books-logo.png"
-    //   alt="ai-books"
-    // ></img>
     <NavLink to="/">
       <h1 className="sm:text-xl text-white">Sound Thoughts Blog</h1>
     </NavLink>
